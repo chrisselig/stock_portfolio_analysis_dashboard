f50e9676c304c6bee8d0ba5ae00f60d6acc1be5d
chrisselig <cselig@hotmail.com>
2020-02-05 20:37:59 -0700

    created clean prices function

--- 02_scripts/01_get_stock_prices.R
+++ 02_scripts/01_get_stock_prices.R
@@ -1,5 +1,7 @@
-# Script is used to get data from selected stock index ----
+# Script is used to load functions related to getting and cleaning stock data ----
 
+
+# Function is used to get data from selected stock index ----
 get_stock_data_function <- function(stockSymbols,
                                     startDate, 
                                     endDate){
@@ -18,11 +20,63 @@ get_stock_data_function <- function(stockSymbols,
     return(prices)
 }
 
-# 
-# # Testing----
-# library(quantmod)
-# library(tidyverse)
-# 
-# symbols <- c("TSLA","AAPL")
-# 
-# get_stock_data_function(stockSymbols = symbols, startDate = "2019-01-01", endDate = "2019-12-31")
+
+calculate_returns_function <- function(data){
+    
+    prices_tidy <- data %>% 
+        data.frame(date = index(.)) %>% 
+        remove_rownames() %>% 
+        pivot_longer(
+            cols = 1:(ncol(.)-1)
+        ) %>% 
+        group_by(name) %>% 
+        mutate(returns = (log(value) - log(lag(value)))) %>% 
+        select(-value) %>% 
+        na.omit() %>% 
+        rename(stock = name)
+    
+    return(prices_tidy)
+    
+}
+
+# Function is used to switch between daily, monthly, and annual data processing ----
+stock_data_tidy_function <- function(stockPrices,
+                                     timePeriod = "monthly"){
+    
+    
+    
+    if(timePeriod == "monthly"){
+        prices_tidy <- prices %>% 
+            to.monthly(indexAt = "lastof", OHLC = FALSE) %>% 
+            calculate_returns_function()
+    } else if(timePeriod == "daily") {
+        prices_tidy <- prices %>% 
+            to.daily(indexAt = "lastof", OHLC = FALSE) %>% 
+            calculate_returns_function()
+    } else if(timePeriod == "weekly"){
+        prices_tidy <- prices %>% 
+            to.weekly(indexAt = "lastof", OHLC = FALSE) %>% 
+            calculate_returns_function()
+    } else {
+        prices_tidy <- prices %>% 
+            to.yearly(indexAt = "lastof", OHLC = FALSE) %>% 
+            calculate_returns_function()
+    }
+    
+    return(prices_tidy)
+}
+
+
+
+ 
+# Testing----
+library(quantmod)
+library(tidyverse)
+library(tibbletime)
+
+symbols <- c("TSLA","AAPL")
+
+prices <- get_stock_data_function(stockSymbols = symbols, startDate = "2015-01-01", endDate = "2019-12-31")
+
+stock_data_tidy_function(stockPrices = prices, timePeriod = "monthly")
+stock_data_tidy_function(stockPrices = prices, timePeriod = "yearly")
